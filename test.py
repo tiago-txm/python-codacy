c7eb3a4f7a372146707047cf2df25cb58222a4f1
Tiago Matos <picpay@PPM-SPO-14879.local>
2021-02-24 08:20:17 -0300

    bagunça identação

--- test.py
+++ test.py
@@ -2,6 +2,14 @@
 `getdoc` should land before `getmodule`
 """
 
+def teste():
+   if True:
+       #blablabla
+     return True
+
+def teste2():
+print("hello")
+
 from inspect import (
     getmodule,
     getdoc,
@@ -11,11 +19,3 @@ from inspect import (
 print(getmodule(getdoc))
 #blabla
 print(getdoc(getmodule))
-
-def teste():
-    if True:
-        #blablabla
-        return True
-
-def teste2():
- print("hello")
