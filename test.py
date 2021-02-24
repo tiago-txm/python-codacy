d62c8804d97874264010dc9b7aafe8afd4674937
Tiago Matos <picpay@PPM-SPO-14879.local>
2021-02-24 08:57:31 -0300

    fds

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 """
 `getdoc` should land before `getmodule`
 """
-class teste():
-    
+
+
 from inspect import (
     getmodule,
     getdoc
 )
+
 def teste():
    if True:
        #blablabla
      return True
 
 def teste2():
-print("hello")
+    print("hello")
 
 
 
